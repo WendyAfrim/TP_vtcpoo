c55439e45cd1c28221773aa87939786be3ab80a2
Wendy <wendy.afrim2@gmail.com>
2020-07-15 12:47:12 +0200

    Function selectOne set

--- classes/conducteurs.class.php
+++ classes/conducteurs.class.php
@@ -18,4 +18,34 @@ class Conducteurs {
         $reqSelect = $this->conn->query("SELECT * FROM conducteurs");
         return $reqSelect;
     }
+
+    public function selectOne () {
+
+        $query = ('SELECT * FROM conducteurs WHERE id_conducteur = :id_detail');
+
+        $reqSelectOne = $this->conn->prepare($query);
+        $reqSelectOne->execute ([
+                                        'id_detail' => $this->id
+        ]);
+
+        $row = $reqSelectOne->fetch();
+
+        $this->prenom = $row['prenom'];
+        $this->nom = $row['nom'];
+        $this->age= $row['age'];
+    }
+
+    public function create() {
+
+        $query = "INSERT INTO conducteurs (prenom, nom, age) VALUES (:prenom, :nom, :age)";
+
+        $reqInsert = $this->conn->prepare($query);
+        $reqInsert->execute(array(
+                              'prenom' => $this->prenom,
+                              'nom' => $this->nom,
+                              'age' => $this->age  
+        ));
+
+        return $reqInsert;
+    }
 }
